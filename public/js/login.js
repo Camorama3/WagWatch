ba30bd5ce16fc7d799f6775f371eef642cddd556
Cameron Fair <cameronfaircellist@gmail.com>
2024-03-18 19:01:57 -0500

    login routes and handlebars fix

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,15 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const userEmail = document.querySelector('#user-owner-email-login').value.trim();
+  const userPassword = document.querySelector('#user-owner-password-login').value.trim();
+  const sitterEmail = document.querySelector('#sitter-email-login').value.trim();
+  const sitterPassword = document.querySelector('#sitter-password-login').value.trim();
 
-  if (email && password) {
+  if (userEmail && userPassword) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ userEmail, userPassword }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -16,6 +18,19 @@ const loginFormHandler = async (event) => {
     } else {
       alert('Failed to log in');
     }
+  }
+};
+if (sitterEmail && sitterPassword) {
+  const response = await fetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ sitterEmail, sitterPassword }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log in');
   }
 };
 
